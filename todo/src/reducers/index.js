7e3853509d18bec0bfda416a425645c5f5eec82d
Kirsten S <kirsten@Kirstens-MacBook-Air.local>
2020-06-17 19:28:07 -0400

    created reducers folder with index.js file, made and exported initialState and simple reducer

---
+++ todo/src/reducers/index.js
@@ -0,0 +1,12 @@
+export const initialState = {
+    item: "",
+    completed: false,
+    id: 0
+}
+
+export const reducer = (state, action) => {
+    switch (action.type) {
+        default: 
+            return state;
+    }
+}
