13c97f32548f4191ac8a4ff002c70c0df5c721a3
Kirsten S <kirsten@Kirstens-MacBook-Air.local>
2020-06-17 20:54:14 -0400

    redid the shape of the state, created case to add todo item, it works now

--- todo/src/components/TodoForm.js
+++ todo/src/components/TodoForm.js
@@ -1,9 +1,33 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-const TodoForm = () => {
+const TodoForm = (props) => {
+    const [newItem, setNewItem] = useState("");
+
+    const handleChanges = e => {
+        setNewItem(e.target.value);
+    }
+
+    const submitForm = e => {
+        e.preventDefault();
+        props.dispatch({ type: 'ADD_ITEM', payload: newItem });
+        setNewItem("");
+    }
+
     return (
         <div>
             <h2>This is the Form</h2>
+            <form>
+                <label htmlFor="todo">Add a Todo</label>
+                <input
+                    type="text"
+                    name="todo"
+                    id="todo"
+                    placeholder="Add a todo..."
+                    value={newItem}
+                    onChange={handleChanges}
+                />
+                <button onClick={submitForm}>Add Todo</button>
+            </form>
         </div>
     )
 }
--- todo/src/reducers/index.js
+++ todo/src/reducers/index.js
@@ -1,15 +1,22 @@
-export const initialState = {
-    todos: [
-        {
-            item: "Sample todo",
-            completed: false,
-            id: 0
-        }
-    ]
-}
+export const initialState = [
+    {
+        item: "Sample todo",
+        completed: false,
+        id: 0
+    }
+]
 
 export const reducer = (state, action) => {
     switch (action.type) {
+        case "ADD_ITEM":
+            return ([ 
+                ...state,
+                {
+                    item: action.payload,
+                    completed: false,
+                    id: Date.now()
+                }
+            ])
         default: 
             return state;
     }
